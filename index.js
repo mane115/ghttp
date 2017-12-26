c6f9984389b9b355c9f04d7fa123f9f230ae7687
gh <guohao0223@gmail.com>
2017-12-26 15:50:24 +0800

    fix bug: 使用错误process属性导致每次都导出es5版本

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // 'use strict'
-// if (process.version && process.version.node) {
-//     let version = process.version.node.split('.');
-//     if (+version[0] > 6) {
-//         module.exports = require('./http');
-//     } else {
-//         module.exports = require('./http.es5');
-//     }
-// } else {
-//     module.exports = require('./http.es5');
-// }
- module.exports = require('./http');
+if (process.versions && process.versions.node) {
+	let version = process.versions.node.split('.');
+	if (+version[0] > 6) {
+		module.exports = require('./http');
+	} else {
+		module.exports = require('./http.es5');
+	}
+} else {
+	module.exports = require('./http.es5');
+}
