db29335e3ffa6d6025ef6b4bfed3bda48a4c2683
gh <guohao0223@gmail.com>
2017-12-26 15:25:00 +0800

    默认返回es6

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-'use strict'
-if (process.version && process.version.node) {
-    let version = process.version.node.split('.');
-    if (+version[0] > 6) {
-        module.exports = require('./http');
-    } else {
-        module.exports = require('./http.es5');
-    }
-} else {
-    module.exports = require('./http.es5');
-}
+// 'use strict'
+// if (process.version && process.version.node) {
+//     let version = process.version.node.split('.');
+//     if (+version[0] > 6) {
+//         module.exports = require('./http');
+//     } else {
+//         module.exports = require('./http.es5');
+//     }
+// } else {
+//     module.exports = require('./http.es5');
+// }
+ module.exports = require('./http.es6');
